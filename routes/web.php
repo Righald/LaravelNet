c6743c178b3e67a3924b77cfb3eb2f36e471e42d
Sergio Rojas <sergio.rojas.cch@gmail.com>
2020-10-14 19:49:28 -0600

    Primera Clase

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test/{id}', function ($id) {
+    return view('test', compact('id'));
+});
+
